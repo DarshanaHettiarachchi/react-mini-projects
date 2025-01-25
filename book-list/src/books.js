81cd24ec41d26fb342937f978c9e471e63f24167
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-01-25 17:34:05 +0530

    images form src for optimization

--- book-list/src/books.js
+++ book-list/src/books.js
@@ -1,14 +1,24 @@
+import book1 from './images/book_1.jpg';
+import book2 from './images/book_2.jpg';
+import book3 from './images/book_3.jpg';
+
 export const books = [
   {
     author: 'Jordan Moore',
     title: 'Interesting Facts For Curious Minds',
-    img: './images/book_1.jpg',
+    img: book1,
     id: 1,
   },
   {
     author: 'James Clear',
     title: 'Atomic Habits',
-    img: './images/book_1.jpg',
+    img: book2,
+    id: 2,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: book3,
     id: 2,
   },
 ];
