ecbbb32ef284e440d6f4a8c790b61cebb158991e
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-01-25 16:58:10 +0530

    Split into files.

---
+++ book-list/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book_1.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: './images/book_1.jpg',
+    id: 2,
+  },
+];
--- book-list/src/index.js
+++ book-list/src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import './index.css';
-
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './images/book_1.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: './images/book_1.jpg',
-    id: 2,
-  },
-];
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
   return (
@@ -24,16 +11,6 @@ const BookList = () => {
         <Book {...book} key={book.id} />
       ))}
     </section>
-  );
-};
-
-const Book = ({ author, title, img }) => {
-  return (
-    <article className='book'>
-      <img src={img} alt='Interesting Facts For Curious Minds' />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
